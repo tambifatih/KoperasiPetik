2df52fd39e5a66d6019610275822be83c265e4c3
petik <tambifatahillah2gmail.com>
2019-04-11 11:06:52 +0700

    bisa

---
+++ app/src/main/java/com/example/petik/koperasipetik/HomePage.java
@@ -0,0 +1,13 @@
+package com.example.petik.koperasipetik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Layout_Hompage);
+    }
+}
